bad06556e21dd49c02a4621ea015b818c2c797c0
Brannon Glover <brannon.glover@warnermedia.com>
2026-03-05 14:46:19 -0500

    fixing netlify issues

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
